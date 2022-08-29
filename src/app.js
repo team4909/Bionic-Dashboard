c76912f8ddf2647eefd27bf1e7e8591f3efe9d26
Mihir Patankar <mpatankar06@gmail.com>
2022-08-29 15:10:04 -0400

    add logo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import { React } from "react";
 import BatteryDisplay from "./components/BatteryDisplay";
+import Logo from "./components/Logo";
 
 function App() {
 	return (
@@ -9,7 +10,11 @@ function App() {
 			<Grid container rowSpacing={10} direction="column">
 				{/* Header */}
 				<Grid item container columnSpacing={2} flexDirection="row">
-					<Grid item xs={10}/>
+					<Grid item xs={4}/>
+					<Grid item xs={4}>
+						<Logo />
+					</Grid>
+					<Grid item xs={2}/>
 					<Grid item xs={2}>
 						<BatteryDisplay />
 					</Grid>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,16 @@
 import { React, Component } from "react";
 import { Box } from "@mui/system";
+import logo from "../assets/Logo.png";
 
 export default class Logo extends Component {
 	render() {
 		return (
-			<Box>Logo</Box>
+			<Box display={"flex"} justifyContent={"center"}>
+				<img src={logo} style={{
+					width: 7076 / 32,
+					height: 3570 / 32,
+				}} />
+			</Box>
 		);
 	}
 }
